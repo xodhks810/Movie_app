e179a176429f760b7b8ed4c054d47de6b6ce50cb
xodhks810 <72534654+xodhks810@users.noreply.github.com>
2020-11-25 17:22:05 +0900

    기본적인 코드 사용법

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -12,7 +13,20 @@ class App extends React.Component {
   minus = () => {
     this.setState(current => ({count: current.count - 1}))
   }
+
+  componentDidMount() { //페이지 로딩
+    console.log("component rendered")
+  }
+
+  componentDidUpdate() {//페이지 데이터 업데이트
+    console.log("I just Updating")
+  }
+
+  componentWillUnmount() {//페이지 로딩 실패
+    console.log("Goodbye, crule world")
+  }
   render() {
+    console.log("I'm rendering")
     return (
       <div>
         <h1> This number is {this.state.count} </h1>
